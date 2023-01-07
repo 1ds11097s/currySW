eb5cc70de5da4b672c6b4471caae422047c34f72
大坪 裕二 <1ds11097s@gmail.com>
2023-01-07 17:19:08 +0900

    a

--- pages/monsters.vue
+++ pages/monsters.vue
@@ -4,7 +4,7 @@
       <div class="headline font-weight-bold my-6" style="font-family: Anton!important;">Monster Report List</div>
       <v-row justify="center" class="main-banner-area rounded-xl pa-4 mb-4">
         <v-col cols="12" style="color:white">
-          <v-btn text color="blue" class="font-weight-bold" href="molong">Mo Long(Water Panda Warrior)</v-btn>
+          <v-btn text color="blue" class="font-weight-bold" href="/molong">Mo Long(Water Panda Warrior)</v-btn>
         </v-col>
       </v-row>
       <template>
