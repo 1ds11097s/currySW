24ca716cb004b11a36302cc6640cfed141b7dd7b
currysw <1ds11097s@gmail.com>
2024-05-04 07:47:41 +0900

    再開

---
+++ .nuxt/dist/server/pages/articles.js
@@ -0,0 +1,288 @@
+exports.ids = [3];
+exports.modules = {
+
+/***/ 107:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VCard/VCard.js + 3 modules
+var VCard = __webpack_require__(87);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VCard/index.js
+var components_VCard = __webpack_require__(86);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VGrid/VCol.js
+var VCol = __webpack_require__(93);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VImg/VImg.js + 2 modules
+var VImg = __webpack_require__(50);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VGrid/VRow.js
+var VRow = __webpack_require__(92);
+
+// CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--7!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/articles.vue?vue&type=template&id=b83d8dc0
+
+
+
+
+
+
+var articlesvue_type_template_id_b83d8dc0_render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c(VRow["a" /* default */], {
+    staticClass: "main-area rounded-xl py-10",
+    attrs: {
+      "justify": "center"
+    }
+  }, [_c(VCol["a" /* default */], {
+    staticClass: "rounded-xl",
+    attrs: {
+      "cols": "11"
+    }
+  }, [_c('div', {
+    staticClass: "headline my-6",
+    staticStyle: {
+      "font-family": "Anton!important"
+    }
+  }, [_c('h1', {
+    staticClass: "h1-font-size"
+  }, [_vm._v("Articles")])]), _vm._v(" "), _vm.$vuetify.breakpoint.mdAndUp ? [_c(VRow["a" /* default */], {
+    staticClass: "main-banner-area rounded-xl pa-4",
+    attrs: {
+      "justify": "center"
+    }
+  }, [_c(VCol["a" /* default */], {
+    attrs: {
+      "cols": "4"
+    }
+  }, [_c(VCard["a" /* default */], {
+    staticStyle: {
+      "height": "100%"
+    },
+    attrs: {
+      "hover": "",
+      "ripple": "",
+      "href": "#1"
+    }
+  }, [_c(VImg["a" /* default */], {
+    staticStyle: {
+      "position": "relative"
+    },
+    attrs: {
+      "src": "article1.png"
+    }
+  }), _vm._v(" "), _c(components_VCard["a" /* VCardSubtitle */], {
+    staticClass: "font-weight-bold"
+  }, [_vm._v("Make turn2 work well, Introduction of methods")])], 1)], 1), _vm._v(" "), _c(VCol["a" /* default */], {
+    attrs: {
+      "cols": "4"
+    }
+  }, [_c(VCard["a" /* default */], {
+    staticStyle: {
+      "height": "100%"
+    },
+    attrs: {
+      "hover": "",
+      "ripple": "",
+      "href": "#2"
+    }
+  }, [_c(VImg["a" /* default */], {
+    staticStyle: {
+      "position": "relative"
+    },
+    attrs: {
+      "src": "article2.png"
+    }
+  }), _vm._v(" "), _c(components_VCard["a" /* VCardSubtitle */], {
+    staticClass: "font-weight-bold"
+  }, [_vm._v("What's important in RTA")])], 1)], 1), _vm._v(" "), _c(VCol["a" /* default */], {
+    attrs: {
+      "cols": "4"
+    }
+  })], 1), _vm._v(" "), _c('div', {
+    staticClass: "headline font-weight-bold my-6",
+    staticStyle: {
+      "font-family": "Anton!important"
+    },
+    attrs: {
+      "id": "1"
+    }
+  }, [_vm._v("Make turn2 work well, Introduction of methods")]), _vm._v(" "), _c(VRow["a" /* default */], {
+    staticClass: "main-banner-area rounded-xl subtitle-1 white--text pa-4 mb-4",
+    attrs: {
+      "justify": "center"
+    }
+  }, [_c('p', [_vm._v("There are several important points to make turn2 work well"), _c('br'), _c('br'), _vm._v("\nAbout the monster"), _c('br'), _vm._v("\nThere are several monsters that are important for turn2"), _c('br'), _vm._v("\nThe essential monsters are leo, karnal, riley, belial, gapsoo"), _c('br'), _vm._v("\n\nOther important monsters are"), _c('br'), _vm._v("\ngiou, minato, xing zhe, aaliyah, bolverk, feng yan, juno, mei hou wang, haegang, nana, woosa, veromos, lutia, mo long. josephine, abellio, verde"), _c('br'), _vm._v("\n\nThe more important monsters you have, the easier it'll be for the turn2 team to work well"), _c('br'), _c('br'), _vm._v("\n\nAbout the runes"), _c('br'), _vm._v("\nOf course you need runes that are fast, but you don't have to use them for every monster."), _c('br'), _vm._v("\nYou need to use a lot of vio and desp runes, so farming with dragons and giants is necessary."), _c('br'), _vm._v("\nTo achieve higher ranks, you need to farm grind and gem with r5."), _c('br'), _c('br'), _vm._v("\n\nAbout artifact"), _c('br'), _vm._v("\nArtifacts are important, but the quality of runes is more important."), _c('br'), _vm._v("\nMy artifact isn't that strong, but I'm still able to compete in the higher ranks."), _c('br'), _vm._v("\nEach monster has its own options, but the priority is low."), _c('br'), _c('br'), _vm._v("\n\nAbout speed"), _c('br'), _vm._v("\nThe speed of the support monster needs to be fast."), _c('br'), _vm._v("\nOther monsters' speed can be low, but their hp, def, res, etc. need to be high."), _c('br'), _vm._v("\nFor example, riley, aaliyah and other support monsters should be fast."), _c('br'), _vm._v("\nUntil around season 10, it was possible to win even if all monsters were slow, but in recent seasons, it's not possible to win."), _c('br'), _vm._v("\nIf the enemy team is the same turn2, it's possible to win, but if they are turn1 or CC team, speed is necessary."), _c('br'), _vm._v("\nIf you don't get your leo banned, it's not a problem, but basically turn1 and CC team players tend to ban more leo."), _c('br'), _vm._v("\nSo, to win in such a situation, you need a monster with high speed.")])]), _vm._v(" "), _c('div', {
+    staticClass: "headline font-weight-bold my-6",
+    staticStyle: {
+      "font-family": "Anton!important"
+    },
+    attrs: {
+      "id": "2"
+    }
+  }, [_vm._v("What's important in RTA")]), _vm._v(" "), _c(VRow["a" /* default */], {
+    staticClass: "main-banner-area rounded-xl headline font-weight-bold pa-4 mb-4",
+    attrs: {
+      "justify": "center"
+    }
+  })] : [_c(VRow["a" /* default */], {
+    staticClass: "main-banner-area rounded-xl pa-4 mt-4",
+    attrs: {
+      "justify": "center"
+    }
+  }, [_c(VCol["a" /* default */], {
+    attrs: {
+      "cols": "12"
+    }
+  }, [_c(VCard["a" /* default */], {
+    staticStyle: {
+      "height": "100%"
+    },
+    attrs: {
+      "hover": "",
+      "ripple": "",
+      "href": "#1"
+    }
+  }, [_c(VImg["a" /* default */], {
+    staticStyle: {
+      "position": "relative"
+    },
+    attrs: {
+      "src": "article1.png"
+    }
+  }), _vm._v(" "), _c(components_VCard["a" /* VCardSubtitle */], {
+    staticClass: "font-weight-bold"
+  }, [_vm._v("Make turn2 work well, Introduction of methods")])], 1)], 1), _vm._v(" "), _c(VCol["a" /* default */], {
+    attrs: {
+      "cols": "12"
+    }
+  }, [_c(VCard["a" /* default */], {
+    staticStyle: {
+      "height": "100%"
+    },
+    attrs: {
+      "hover": "",
+      "ripple": "",
+      "href": "#2"
+    }
+  }, [_c(VImg["a" /* default */], {
+    staticStyle: {
+      "position": "relative"
+    },
+    attrs: {
+      "src": "article2.png"
+    }
+  }), _vm._v(" "), _c(components_VCard["a" /* VCardSubtitle */], {
+    staticClass: "font-weight-bold"
+  }, [_vm._v("What's important in RTA")])], 1)], 1), _vm._v(" "), _c(VCol["a" /* default */], {
+    attrs: {
+      "cols": "12"
+    }
+  })], 1), _vm._v(" "), _c('div', {
+    staticClass: "headline font-weight-bold my-6",
+    staticStyle: {
+      "font-family": "Anton!important"
+    },
+    attrs: {
+      "id": "1"
+    }
+  }, [_vm._v("Make turn2 work well, Introduction of methods")]), _vm._v(" "), _c(VRow["a" /* default */], {
+    staticClass: "main-banner-area rounded-xl subtitle-1 white--text pa-4 mb-4",
+    attrs: {
+      "justify": "center"
+    }
+  }, [_c('p', [_vm._v("There are several important points to make turn2 work well"), _c('br'), _c('br'), _vm._v("\nAbout the monster"), _c('br'), _vm._v("\nThere are several monsters that are important for turn2"), _c('br'), _vm._v("\nThe essential monsters are leo, karnal, riley, belial, gapsoo"), _c('br'), _vm._v("\n\nOther important monsters are"), _c('br'), _vm._v("\ngiou, minato, xing zhe, aaliyah, bolverk, feng yan, juno, mei hou wang, haegang, nana, woosa, veromos, lutia, mo long. josephine, abellio, verde"), _c('br'), _vm._v("\n\nThe more important monsters you have, the easier it'll be for the turn2 team to work well"), _c('br'), _c('br'), _vm._v("\n\nAbout the runes"), _c('br'), _vm._v("\nOf course you need runes that are fast, but you don't have to use them for every monster."), _c('br'), _vm._v("\nYou need to use a lot of vio and desp runes, so farming with dragons and giants is necessary."), _c('br'), _vm._v("\nTo achieve higher ranks, you need to farm grind and gem with r5."), _c('br'), _c('br'), _vm._v("\n\nAbout artifact"), _c('br'), _vm._v("\nArtifacts are important, but the quality of runes is more important."), _c('br'), _vm._v("\nMy artifact isn't that strong, but I'm still able to compete in the higher ranks."), _c('br'), _vm._v("\nEach monster has its own options, but the priority is low."), _c('br'), _c('br'), _vm._v("\n\nAbout speed"), _c('br'), _vm._v("\nThe speed of the support monster needs to be fast."), _c('br'), _vm._v("\nOther monsters' speed can be low, but their hp, def, res, etc. need to be high."), _c('br'), _vm._v("\nFor example, riley, aaliyah and other support monsters should be fast."), _c('br'), _vm._v("\nUntil around season 10, it was possible to win even if all monsters were slow, but in recent seasons, it's not possible to win."), _c('br'), _vm._v("\nIf the enemy team is the same turn2, it's possible to win, but if they are turn1 or CC team, speed is necessary."), _c('br'), _vm._v("\nIf you don't get your leo banned, it's not a problem, but basically turn1 and CC team players tend to ban more leo."), _c('br'), _vm._v("\nSo, to win in such a situation, you need a monster with high speed.")])]), _vm._v(" "), _c('div', {
+    staticClass: "headline font-weight-bold my-6",
+    staticStyle: {
+      "font-family": "Anton!important"
+    },
+    attrs: {
+      "id": "2"
+    }
+  }, [_vm._v("What's important in RTA")]), _vm._v(" "), _c(VRow["a" /* default */], {
+    staticClass: "main-banner-area rounded-xl headline font-weight-bold pa-4 mb-4",
+    attrs: {
+      "justify": "center"
+    }
+  })], _vm._v(" "), [_c('adsbygoogle', {
+    attrs: {
+      "ad-slot": "9249275840"
+    }
+  })]], 2)], 1);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/articles.vue?vue&type=template&id=b83d8dc0
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/articles.vue?vue&type=script&lang=js
+/* harmony default export */ var articlesvue_type_script_lang_js = ({
+  layout: "default",
+  head() {
+    return {
+      title: "Articles - CurrySW RTA Room【Summoners War】"
+    };
+  }
+});
+// CONCATENATED MODULE: ./pages/articles.vue?vue&type=script&lang=js
+ /* harmony default export */ var pages_articlesvue_type_script_lang_js = (articlesvue_type_script_lang_js); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(7);
+
+// CONCATENATED MODULE: ./pages/articles.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_articlesvue_type_script_lang_js,
+  articlesvue_type_template_id_b83d8dc0_render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "014873a1"
+  
+)
+
+/* harmony default export */ var articles = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 86:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* unused harmony export VCardActions */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return VCardSubtitle; });
+/* unused harmony export VCardText */
+/* unused harmony export VCardTitle */
+/* harmony import */ var _VCard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(87);
+/* harmony import */ var _util_helpers__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1);
+
+
+const VCardActions = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "f"])('v-card__actions');
+const VCardSubtitle = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "f"])('v-card__subtitle');
+const VCardText = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "f"])('v-card__text');
+const VCardTitle = Object(_util_helpers__WEBPACK_IMPORTED_MODULE_1__[/* createSimpleFunctional */ "f"])('v-card__title');
+
+/* unused harmony default export */ var _unused_webpack_default_export = ({
+  $_vuetify_subcomponents: {
+    VCard: _VCard__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"],
+    VCardActions,
+    VCardSubtitle,
+    VCardText,
+    VCardTitle
+  }
+});
+
+/***/ })
+
+};;
+//# sourceMappingURL=articles.js.map
